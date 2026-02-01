24fe09691bb997aa78cd4e6896a78f6e47cc70c2
Pau <pau.bateria@gmail.com>
2026-02-01 19:27:15 +0100

    BookService básico para prueba

---
+++ Application/Services/BookService.cs
@@ -0,0 +1,31 @@
+﻿using Application.Dtos;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<List<BookDto>> GetAllBooksAsync()
+        {
+            var books = await _bookRepository.GetAllBooks();
+            var librosDto = books.Select(l => new BookDto
+            {
+                Id = l.Id,
+                Titulo = l.Titulo
+            });
+
+            return [.. librosDto];
+        }
+    }
+}
