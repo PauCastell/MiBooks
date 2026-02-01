5f13df2aadc53e7658ad339baa3a250d7a3506c3
Pau <pau.bateria@gmail.com>
2026-02-01 19:26:00 +0100

    Interfaces de application

---
+++ Application/Interfaces/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces
+{
+    public interface IBookService
+    {
+        Task<List<BookDto>> GetAllBooksAsync();
+    }
+}
---
+++ Application/Interfaces/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces
+{
+    public interface IFileService
+    {
+        Task<List<FileBookDto>> GetFileBookFromPathAsync(string path);
+        Task<string> RenameFileBookAsync(string path, string bookTitle, string authorName);
+        Task MoveFileBook(string originPath, string destinationPath);
+    }
+}
