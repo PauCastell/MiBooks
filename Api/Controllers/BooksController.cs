4e28933a31f88c5b3e3629a3694ec67f89081c7e
Pau <pau.bateria@gmail.com>
2026-02-01 19:25:32 +0100

    Crear controller mínimo para prueba

---
+++ Api/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController: ControllerBase
+    {
+        private readonly IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBooksAsync()
+        {
+            var books = await _bookService.GetAllBooksAsync();
+             return Ok(books);
+        }
+    }
+}
