a9cd97ee69855c05d3dc132237d503b955680a90
Pau <pau.bateria@gmail.com>
2026-02-01 19:28:00 +0100

    Crear dtos para book y filebook

---
+++ Application/Dtos/BookDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Dtos
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+    }
+}
---
+++ Application/Dtos/FileBookDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Dtos
+{
+    public class FileBookDto
+    {
+        public string FileName { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string Path { get; set; }
+    }
+}
