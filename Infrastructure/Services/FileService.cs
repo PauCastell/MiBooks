269530e45b11196ce498ecfd28265a5b5fe7f9d4
Pau <pau.bateria@gmail.com>
2026-02-08 17:58:33 +0100

    Implementar File Service

--- Infrastructure/Services/FileService.cs
+++ Infrastructure/Services/FileService.cs
@@ -1,25 +1,46 @@
 ﻿using Application.Interfaces;
-using MyBooks.Application.Dtos;
+using MyBooks.Shared.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MyBooks.Infrastructure.Services
 {
+    //TODO: Implementar control de errores y validaciones.
     public class FileService : IFileService
     {
-        public Task<List<FileBookDto>> GetFileBookFromPathAsync(string path, string extension)
+        public List<FileBookDto> GetFileBookFromPath(string path, string extension)
         {
-            var files = Directory.GetFiles(path, extension);
-
+            var files = GetFilesFromPath(path, extension);
+            var fileBookDtos = new List<FileBookDto>();
+            foreach (var file in files)
+            {
+                var fileBookDto = MapFilestoFileBookDto(file);
+                fileBookDtos.Add(fileBookDto);
+            }
+            return fileBookDtos;
         }
 
-        private string[] getFilesFromPath(string path, string extension)
+        private string[] GetFilesFromPath(string path, string extension)
         {
             return Directory.GetFiles(path, extension);
         }
 
+        private FileBookDto MapFilestoFileBookDto(string file)
+        {
+            var fileWithoutExtension = Path.GetFileNameWithoutExtension(file);
+            var fileParts = fileWithoutExtension.Split('-');
 
+            return new FileBookDto
+            {
+                FileName = Path.GetFileName(file),
+                Title = fileParts.Length == 2 ? fileParts[0].Trim() : null,
+                Author = fileParts.Length == 2 ? fileParts[1].Trim() : null,
+                Path = file
+            };
+        }
+
+        
 
         public Task MoveFileBook(string originPath, string destinationPath)
         {
