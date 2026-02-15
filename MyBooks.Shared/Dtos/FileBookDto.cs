5d387eae3176bd87bb39ed3c5fd3dbf9a42363a1
Pau <pau.bateria@gmail.com>
2026-02-15 18:06:39 +0100

    Dto compartido

--- MyBooks.Shared/Dtos/FileBookDto.cs
+++ MyBooks.Shared/Dtos/FileBookDto.cs
@@ -6,9 +6,9 @@ namespace MyBooks.Shared.Dtos
 {
     public class FileBookDto
     {
-        public string FileName { get; set; }
-        public string? Title { get; set; }
-        public string? Author { get; set; }
-        public string Path { get; set; }
+        public string FileName { get; set; } = string.Empty;
+        public string? Title { get; set; } = string.Empty;
+        public string? Author { get; set; } = string.Empty;
+        public string Path { get; set; } = string.Empty;
     }
 }
