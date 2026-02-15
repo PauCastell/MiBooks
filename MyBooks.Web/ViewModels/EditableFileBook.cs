557327b4cec37ecf9284f8c2306f29c7b10df031
Pau <pau.bateria@gmail.com>
2026-02-15 18:06:25 +0100

    Crear view model para la ui

--- MyBooks.Web/ViewModels/EditableFileBook.cs
+++ MyBooks.Web/ViewModels/EditableFileBook.cs
@@ -5,8 +5,7 @@
         public string FileName { get; set; }
         public string? Title { get; set; }
         public string? Author { get; set; }
-
         public bool IsSelected { get; set; } = false;
-        public bool IsValid => !string.IsNullOrWhiteSpace(Title) && !string.IsNullOrWhiteSpace(Author);
+
     }
 }
