35cf50d8bf3ff3bad750c79e637a1a70c3a488a1
Pau <pau.bateria@gmail.com>
2026-02-22 18:30:48 +0100

    Crear modelo para editar el libro input para la vista

---
+++ MyBooks.Web/Models/BookImportedVM.cs
@@ -0,0 +1,26 @@
+﻿using MyBooks.Shared.Dtos;
+
+namespace MyBooks.Web.Models
+{
+    public class BookImportedVM
+    {
+        public FileBookDto OriginalData { get; set; }
+
+        public BookImportedVM(FileBookDto originalData)
+        {
+            OriginalData = originalData;
+            Title = string.IsNullOrEmpty(originalData.Title) ? "" : originalData.Title;
+            Author = string.IsNullOrEmpty(originalData.Author) ? "" : originalData.Author;
+            FileName = originalData.FileName;
+        }
+
+        public string FileName { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string? ExternalApiTitle { get; set; }
+        public int? PublishYear { get; set; }
+        public int? PageNumber { get; set; }
+        public bool isValid => !string.IsNullOrEmpty(Title) && !string.IsNullOrEmpty(Author);
+
+    }
+}
