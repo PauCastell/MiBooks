dd455ba8aaeac09e2387b77a0d88f1b453835726
Pau <pau.bateria@gmail.com>
2026-02-28 17:58:08 +0100

    Dtos para mapear google books api

---
+++ Application/Dtos/GoogleBooksDtos/GoogleBooksResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBooks.Application.Dtos.GoogleBooksDtos
+{
+    public class GoogleBooksResponse
+    {
+        public List<GoogleBookItem> Items { get; set; }
+    }
+}
