08a9f3d0b19c46fbac08cc89d4202e7e43a36260
Pau <pau.bateria@gmail.com>
2026-03-01 17:33:01 +0100

    Clase para constantes del proyecto

---
+++ MyBooks.Shared/Const/ConstValues.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBooks.Shared.Const
+{
+    public static class ConstValues
+    {
+        public const string GoogleQueryPrefix = "?q=";
+        public const string GoogleIntitle = "intitle:";
+        public const string GoogleInAuthor = "inauthor:";
+        public const string Googlekey = "&key=";
+    }
+}
