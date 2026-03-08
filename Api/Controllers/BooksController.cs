1c95daca21485d367c463a86595e2c17f074e546
Pau <pau.bateria@gmail.com>
2026-03-08 19:35:54 +0100

    Endpoint google-search

--- Api/Controllers/BooksController.cs
+++ Api/Controllers/BooksController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Interfaces;
 using MyBooks.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
+using MyBooks.Application.Interfaces;
 
 namespace Api.Controllers
 {
@@ -9,10 +10,12 @@ namespace Api.Controllers
     public class BooksController: ControllerBase
     {
         private readonly IBookService _bookService;
+        private readonly IGoogleBooksService _googleBooksService;
 
-        public BooksController(IBookService bookService)
+        public BooksController(IBookService bookService, IGoogleBooksService googleBookService)
         {
             _bookService = bookService;
+            _googleBooksService = googleBookService;
         }
 
         [HttpGet]
@@ -20,6 +23,21 @@ namespace Api.Controllers
         {
             var books = await _bookService.GetAllBooksAsync();
              return Ok(books);
+        }
+
+
+        //TODO: Posibilidad de usar un request para más control.
+        [HttpGet("google-search")]
+        public async Task<IActionResult> SearchGoogleBooks([FromQuery] string title, [FromQuery] string author)
+        {
+            if(string.IsNullOrEmpty(title) && string.IsNullOrEmpty(author))
+            {
+                return BadRequest("Se debe proporcionar al menos un título o autor");
+            }
+
+            var books = await _googleBooksService.GetBookByTitleAndAuthor(title, author);
+
+            return Ok(books);
         }
     }
 }
