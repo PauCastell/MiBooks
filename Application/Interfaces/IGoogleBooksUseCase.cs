53af0a7ba5b8332334d8af724d0e3553561a181c
Pau <pau.bateria@gmail.com>
2026-03-08 19:35:19 +0100

    Casos de uso para consultas google books

---
+++ Application/Interfaces/IGoogleBooksUseCase.cs
@@ -0,0 +1,12 @@
+﻿using MyBooks.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBooks.Application.Interfaces
+{
+    public interface IGoogleBooksUseCase
+    {
+        Task<List<GoogleBookDto>> GetBookByTitleAndAuthor(string title, string author);
+    }
+}
---
+++ Application/Services/GoogleBooksUseCase.cs
@@ -0,0 +1,31 @@
+﻿using MyBooks.Application.Dtos;
+using MyBooks.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace MyBooks.Application.Services
+{
+    public class GoogleBooksUseCase : IGoogleBooksUseCase
+    {
+        private readonly IGoogleBooksService _googleBooksService;
+        public GoogleBooksUseCase(IGoogleBooksService googleBookService)
+        { 
+            _googleBooksService = googleBookService;
+        }
+
+        public async Task<List<GoogleBookDto>> GetBookByTitleAndAuthor(string title, string author)
+        {
+            title = title.Trim();
+            author = author.Trim();
+
+            if(string.IsNullOrEmpty(title) && string.IsNullOrEmpty(author))
+            {
+                return [];
+            }
+
+            return await _googleBooksService.GetBookByTitleAndAuthor(title, author);
+        }
+    }
+}
