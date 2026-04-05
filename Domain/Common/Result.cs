9694d02e97d4a42e48e9dfe7068c72b77002f867
Pau <pau.bateria@gmail.com>
2026-04-05 11:10:31 +0200

    Modelo para envolver el resultado de las consultas

---
+++ Domain/Common/Result.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBooks.Domain.Common;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; private set; }
+    public string Message { get; private set; } = string.Empty;
+    public T? Data { get; private set; }
+
+    public static Result<T> Success (T data)
+    {
+        return new Result<T>
+        {
+            IsSuccess = true,
+            Data = data
+        };
+    }
+
+    public static Result<T> Failure (string message)
+    {
+        return new Result<T>
+        {
+            IsSuccess = false,
+            Message = message
+        };
+    }
+}
