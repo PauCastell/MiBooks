da55536417a16586ac3132fe8cdf17b46e7dc47d
Pau <pau.bateria@gmail.com>
2026-04-06 17:48:44 +0200

    Añadir métodos a la interface

--- Application/Interfaces/IBookRepository.cs
+++ Application/Interfaces/IBookRepository.cs
@@ -8,5 +8,9 @@ namespace MyBooks.Application.Interfaces
     public interface IBookRepository
     {
         Task<List<LibroRefactor>> GetAllBooks();
+        Task<Autor?> GetAutorByNameAsync(string name);
+        Task<LibroRefactor> AddBookAsync(LibroRefactor book);
+        Task<Autor> AddAutorAsync(Autor autor);
+        Task<LibroRefactor?> GetBookByTitle(string title);
     }
 }
